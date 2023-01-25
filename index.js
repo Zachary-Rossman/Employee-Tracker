a6d92b3f27ee0e661a6b404a6add00ff2390e038
Zachary-Rossman <zrossman@yahoo.com>
2023-01-25 16:22:55 -0600

    added console.table to index.js; now not functioning

--- index.js
+++ index.js
@@ -1,38 +1,40 @@
 const inquirer = require("inquirer");
 const mysql = require("mysql2");
+const cTable = require("console.table");
 
 const db = mysql.createConnection(
     {
-        host: 'localhost',
-        // MySQL username,
-        user: 'root',
-        //MySQL password
-        password: 'root',
-        database: 'tracker'
+        host: "localhost",
+        // MySQL username
+        user: "root",
+        // MySQL password
+        password: "root",
+        database: "tracker",
     },
-    console.log('Connected to the tracker database')
+    console.log('Connected to the tracker database.')
 );
 
-// Query database
-db.query('SELECT * FROM students', function (err, results) {
-    console.log(results);
-});
-
 function viewDepartments(){
-    console.log("Success")
-;}
+    db.query("SELECT * FROM department", function (err, results) {
+        console.table(results);
+    })
+}
 
 inquirer
-.prompt([
+  .prompt([
     {
         type: "list",
         message: "What would you like to do?",
         name: "choice",
-        choices: [{ name: "View all department", value: "VIEW_DEPARTMENTS"}],
+        choices: [
+            { name: "View all departments", value: "VIEW DEPARTMENTS" },
+            { name: "View all employees", value: "VIEW EMPLOYEES" },
+            { name: "View all roles", value: "VIEW ROLES" },
+        ],
     },
 ])
 .then((response) => {
-    if (response.choice === "VIEW_DEPARTMENTS"){
+    if (response.choice === "VIEW DEPARTMENTS") {
         viewDepartments();
     }
 });
