6695e198ff8f388139f49c186b31d270161f1ae5
Zachary-Rossman <zrossman@yahoo.com>
2023-01-26 10:43:22 -0600

    added all functions, function calls, and inquirer options; was functioning, computer updated and inquirer ending app before allowing selection

--- index.js
+++ index.js
@@ -19,13 +19,28 @@ function viewDepartments(){
         console.table(results);
     })
 };
-function viewEmployees(){
-    db.query("SELECT * FROM employee", function (err, results) {
+function viewRoles(){
+    db.query("SELECT * FROM role", function (err, results) {
         console.table(results);
     })
 };
-function viewRoles(){
-    db.query("SELECT * FROM role", function (err, results) {
+function viewEmployees(){
+    db.query("SELECT * FROM employees", function (err, results) {
+        console.table(results);
+    })
+};
+function addDepartment(){
+    db.query("INSERT INTO department", function (err, results) {
+        console.table(results);
+    })
+};
+function addRole(){
+    db.query("INSERT INTO role", function (err, results) {
+        console.table(results);
+    })
+};
+function addEmployee(){
+    db.query("INSERT INTO employee", function (err, results) {
         console.table(results);
     })
 };
@@ -37,20 +52,50 @@ inquirer
         message: "What would you like to do?",
         name: "choice",
         choices: [
-            { name: "View all departments", value: "VIEW DEPARTMENTS" },
-            { name: "View all employees", value: "VIEW EMPLOYEES" },
-            { name: "View all roles", value: "VIEW ROLES" },
-        ],
-    },
-])
-.then((response) => {
+            {
+                name: "View all deparments",
+                value: "VIEW DEPARTMENTS"
+            },
+            {
+                name: "View all roles",
+                value: "VIEW ROLES"
+            },
+            {
+                name: "View all employees",
+                value: "VIEW EMPLOYEES"
+            },
+            {
+                name: "Add new department",
+                value: "ADD DEPARTMENT"
+            },
+            {
+                name: "Add new role",
+                value: "ADD ROLE"
+            },
+            {
+                name: "Add new employee", 
+                value: "ADD EMPLOYEE" 
+            }
+        ]
+    }
+  ])
+  .then((response) => {
     if (response.choice === "VIEW DEPARTMENTS") {
         viewDepartments();
-    }
-    if (response.choice === "VIEW EMPLOYEES") {
-        viewEmployees();
     }
     if (response.choice === "VIEW ROLES") {
         viewRoles();
     }
-});
+    if (response.choice === "VIEW EMPLOYEES") {
+        viewEmployees();
+    }
+    if (response.choice === "ADD DEPARTMENT") {
+        addDepartment();
+    }
+    if (response.choice === "ADD ROLE") {
+        addRole();
+    }
+    if (response.choice === "ADD EMPLOYEE") {
+        addEmployee();
+    }
+  })
